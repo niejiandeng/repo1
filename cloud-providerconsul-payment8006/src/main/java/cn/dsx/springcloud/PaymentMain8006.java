21e5e39e8e7d8d8edc1c5b21911d93b1783d4a1b
DouShaoxun <dsx1046811165@gmail.com>
2020-04-12 20:52:04 +0800

    consul 服务提供者

---
+++ cloud-providerconsul-payment8006/src/main/java/cn/dsx/springcloud/PaymentMain8006.java
@@ -0,0 +1,20 @@
+package cn.dsx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Classname: PaymentMain8006
+ * @Author: Dsx
+ * @Date: 2020/04/12/20:50
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/cn/dsx/springcloud/config/PaymentController.java
@@ -0,0 +1,28 @@
+package cn.dsx.springcloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Classname: PaymentController
+ * @Author: Dsx
+ * @Date: 2020/04/12/20:50
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @RequestMapping("/payment/consul")
+    public String paymentConsul() {
+        return "springCloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
