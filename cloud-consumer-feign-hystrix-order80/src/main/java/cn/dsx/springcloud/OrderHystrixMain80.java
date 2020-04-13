ed49ee3013797ca5d8835b5b46f8a862e3937679
DouShaoxun <dsx1046811165@gmail.com>
2020-04-13 11:19:03 +0800

    FallbackMethod

--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/dsx/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/dsx/springcloud/OrderHystrixMain80.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * 服务降级是为了在服务繁忙或者报错的时候 给用户一个友好的提示
+ * 服务熔断 是达到最大访问量之后 直接拒绝访问    服务熔断会导致服务降级  当访问量降低之后 继续接收访问请求
+ * 服务限流  限制规定时间内 只能有多少个用户访问该服务
  * @Classname: OrderHystrixMain80
  * @Author: Dsx
  * @Date: 2020/04/13/10:28
--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/dsx/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/dsx/springcloud/service/PaymentHystrixService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * 客户端配置
  * fallback指定 服务降级方法
+ * 这里处理有一个好处 对每一个方法都针对性的做一个 服务降级  达到解耦的目的
  * @Classname: PaymentHystrixService
  * @Author: Dsx
  * @Date: 2020/04/13/10:30
