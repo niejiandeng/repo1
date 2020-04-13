1edaa10120c0ff9c3eaf4f406b377983f6014bc2
DouShaoxun <dsx1046811165@gmail.com>
2020-04-13 11:58:33 +0800

    HystrixDashboard 监控 http://localhost:9001/hystrix

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/cn/dsx/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package cn.dsx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Classname: HystrixDashboardMain9001
+ * @Author: Dsx
+ * @Date: 2020/04/13/11:55
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
