1c65f00bb955c76103c09f080a6651ea61b47c2f
DouShaoxun <dsx1046811165@gmail.com>
2020-04-14 09:38:36 +0800

    代码格式化

--- cloud-config-client-3355/src/main/java/cn/dsx/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/cn/dsx/springcloud/ConfigClientMain3355.java
@@ -11,8 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
-public class ConfigClientMain3355
-{
+public class ConfigClientMain3355{
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientMain3355.class, args);
     }
