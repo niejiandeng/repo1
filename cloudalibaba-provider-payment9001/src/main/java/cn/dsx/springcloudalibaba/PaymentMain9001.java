5fd6585f5f296ca6846e5fdeb0a80501698f0044
DouShaoxun <dsx1046811165@gmail.com>
2020-04-15 10:10:01 +0800

    nacos-payment-provider

---
+++ cloudalibaba-provider-payment9001/src/main/java/cn/dsx/springcloudalibaba/PaymentMain9001.java
@@ -0,0 +1,20 @@
+package cn.dsx.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Classname: PaymentMain9001
+ * @Author: Dsx
+ * @Date: 2020/04/15/10:08
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
+
---
+++ cloudalibaba-provider-payment9001/src/main/java/cn/dsx/springcloudalibaba/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package cn.dsx.springcloudalibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname: PaymentController
+ * @Author: Dsx
+ * @Date: 2020/04/15/10:08
+ */
+@RestController
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
