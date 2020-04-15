0e780d213fe557e02a7b0f6a5360bda6726c306b
DouShaoxun <dsx1046811165@gmail.com>
2020-04-15 11:17:05 +0800

    nacos config

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/dsx/springcloudalibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package cn.dsx.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Classname: NacosConfigClientMain3377
+ * @Author: Dsx
+ * @Date: 2020/04/15/11:07
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/dsx/springcloudalibaba/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package cn.dsx.springcloudalibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname: ConfigClientController
+ * @Author: Dsx
+ * @Date: 2020/04/15/11:08
+ */
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
+
