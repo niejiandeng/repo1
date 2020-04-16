d5885b99d3ef0ca0b0df2ff5bae231b9d572b8e2
DouShaoxun <dsx1046811165@gmail.com>
2020-04-16 09:23:01 +0800

    sentinel 监控   HotKey规则

--- cloudalibaba-sentinel-service8401/src/main/java/cn/dsx/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/dsx/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,10 @@
 package cn.dsx.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/04/15/20:32
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
 
@@ -20,5 +25,35 @@ public class FlowLimitController {
     @GetMapping("b")
     public String returnB() {
         return "b";
+    }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+
+        log.info("testD 异常比例");
+        int age = 10/0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")//blockHandler 如果不配 默认显示错误页面
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        return "------testHotKey";
+    }
+    public String deal_testHotKey (String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
     }
 }
