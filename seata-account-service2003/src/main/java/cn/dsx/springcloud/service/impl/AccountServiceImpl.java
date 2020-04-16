299cff664a306a96b317894148e539720bba425c
DouShaoxun <dsx1046811165@gmail.com>
2020-04-16 16:14:09 +0800

    seata  GlobalTransactional 分布式事务

--- seata-account-service2003/src/main/java/cn/dsx/springcloud/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/cn/dsx/springcloud/service/impl/AccountServiceImpl.java
@@ -26,7 +26,11 @@ public class AccountServiceImpl implements AccountService {
         log.info("------->account-service中扣减账户余额开始");
         //模拟超时异常，全局事务回滚
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         accountMapper.decrease(userId,money);
         log.info("------->account-service中扣减账户余额结束");
     }
--- seata-order-service2001/src/main/java/cn/dsx/springcloudalibaba/controller/OrderController.java
+++ seata-order-service2001/src/main/java/cn/dsx/springcloudalibaba/controller/OrderController.java
@@ -3,7 +3,8 @@ package cn.dsx.springcloudalibaba.controller;
 import cn.dsx.springcloud.entities.CommonResult;
 import cn.dsx.springcloudalibaba.domain.Order;
 import cn.dsx.springcloudalibaba.service.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -18,8 +19,8 @@ public class OrderController{
     @Resource
     private OrderService orderService;
 
-    @GetMapping("/order/create")
-    public CommonResult create(Order order)    {
+    @PostMapping("/order/create")
+    public CommonResult create(@RequestBody Order order)    {
         orderService.create(order);
         return new CommonResult(200,"订单创建成功");
     }
